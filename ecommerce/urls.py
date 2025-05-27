70aa72668ab9f252a4f906b3bd8559a4da11ab4c
ckotkar8860 <chirayukotkar99@gmail.com>
2025-05-27 16:42:27 +0530

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the ecommerce site!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
+    path('', home),  # Add this line for the root path
 ]
